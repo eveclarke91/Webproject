4f6e00d8f5d041ceee12a44eaf88b196ad55fd0b
Yvonne Clarke <eveclarke91@gmail.com>
2015-11-13 19:49:46 +0000

    WSP Project - 2015

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+      def show
+        @user = User.find(params[:id])
+      end
+      
+  def new
+  	 @user = User.new
+  end
+
+  def create
+        secure_params = params.require(:user).permit(:name, :email, 
+                                  :password, :password_confirmation)
+        @user = User.new(secure_params)
+        if @user.save
+          remember @user       #  NEW LINE
+          flash[:success] = "Welcome to the Sample App!"  
+        	flash[:success] = "Welcome to the Twitter App!"
+        	redirect_to @user
+           # Handle a successful save.
+        else
+            render 'new'     
+        end
+      end
+  end
